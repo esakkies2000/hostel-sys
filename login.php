80c4472581a23f3dfd5267d1b9f9b1441c63d7a5
esakkies2000 <69142111+esakkies2000@users.noreply.github.com>
2021-11-02 22:13:28 +0530

    Update login.php

--- login.php
+++ login.php
@@ -20,11 +20,11 @@ $stmt=$mysqli->prepare("SELECT email,password,id FROM userregistration WHERE ema
              $uid=$_SESSION['id'];
              $uemail=$_SESSION['login'];
 $ip=$_SERVER['REMOTE_ADDR'];
-$geopluginURL='http://www.geoplugin.net/php.gp?ip='.$ip;
+$geopluginURL='https://www.geoplugin.net/php.gp?ip='.$ip;
 $addrDetailsArr = unserialize(file_get_contents($geopluginURL));
 $city = $addrDetailsArr['geoplugin_city'];
 $country = $addrDetailsArr['geoplugin_countryName'];
-$log="insert into userLog(userId,userEmail,userIp,city,country) values('$uid','$uemail','$ip','$city','$country')";
+$log="insert into userlog(userId,userEmail,userIp,city,country) values('$uid','$uemail','$ip','$city','$country')";
 $mysqli->query($log);
 if($log)
 {
