01aa73e6baeaf50d3865e7691c34d777eab268db
esakkies2000 <69142111+esakkies2000@users.noreply.github.com>
2021-11-02 22:28:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 session_start();
-<?php
 $dbuser="8tORhkIRdT";
 $dbpass="BFuG1RXoBm";
 $host="remotemysql.com";
 $db="8tORhkIRdT";
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
-?>
 if(isset($_POST['login']))
 {
 $email=$_POST['email'];
