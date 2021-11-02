ade873b416b469c1f77b44890441893f5e0bcbce
esakkies2000 <69142111+esakkies2000@users.noreply.github.com>
2021-11-02 22:42:50 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,6 @@
 <?php
-$dbuser="8tORhkIRdT";
-$dbpass="BFuG1RXoBm";
-$host="remotemysql.com";
-$db="8tORhkIRdT";
-$mysqli =new mysqli($host,$dbuser, $dbpass, $db);
-?>
-<?php
 session_start();
+include("includes/config.php");
 if(isset($_POST['login']))
 {
 $email=$_POST['email'];
