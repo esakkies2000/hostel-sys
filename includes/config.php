ebe485d6d80bca05977b71d6e5b1909860a88170
esakkies2000 <69142111+esakkies2000@users.noreply.github.com>
2021-11-02 20:36:25 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="hostel";
+$dbuser="8tORhkIRdT";
+$dbpass="BFuG1RXoBm";
+$host="remotemysql.com";
+$db="8tORhkIRdT";
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
 ?>
